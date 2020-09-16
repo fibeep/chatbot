cf67ba4b7a49d50da11a4743ef98845303bcde9b
Salo <salomonco@gmail.com>
2020-09-16 17:08:09 -0400

    first 2 lists done

--- chat.py
+++ chat.py
@@ -4,10 +4,21 @@
 from random import choice
 
 def get_activity_response(user_input):
+
+    #These are the awnsers the bot will give when the user has little to no energy
+    bot_response_0_2 = ["Go sleep","Drink a coffe","Drink an energy drink","Splash some cold water in your face"]
+    #These are the awnsers the bot will give when the user has barely any energy
+    bot_response_3_4 = ["Read a book","Watch a movie","Play a board game","Play a card game"]
+    #These are the awnsers the bot will give when the user has average energy
+    bot_response_5_6 = []
+    #These are the awnsers the bot will give when the user has above average energy
+    bot_response_7_8 = []
     #These are the awnsers the bot will give when the user has a lot of energy
-    bot_response_8_10 = ["Go for a swimm","Go to the gym","Go play your favorite sport","Go jogging","Go skateboarding",]
-    if int(user_input) in range(8,11):
-        return choice(bot_response_8_10)
+    bot_response_9_10 = ["Go for a swimm","Go to the gym","Go play your favorite sport","Go jogging","Go skateboarding",]
+    if int(user_input) in range(0,3):
+        return choice (bot_response_0_2)
+    elif int(user_input) in range(9,11):
+        return choice(bot_response_9_10)
     else:
         return ("Please add an awnser within the parameters")
 
@@ -21,5 +32,6 @@ while user_input != 100 :
     bot_response = get_activity_response(user_input)
     print(bot_response)
     
+
 Bye = "Thank you for participating"
 print(Bye)
